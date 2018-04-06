495f9efc08ccc145094a9c142e0a1ff12850152d
NathanMH <nathan@musicnate.ca>
2018-04-06 09:41:29 -0400

    Try to gitignore output

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,16 +20,17 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('Pronto Musica', 'https://www.prontomusica.org/'),
+         ('Lara Deutsch', 'https://www.laradeutsch.ca/'))
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('twitter', 'https://twitter.com/NathanMH'),
+          ('github', 'https://github.com/NathanMH'))
 
-DEFAULT_PAGINATION = 10
+DEFAULT_PAGINATION = False
+
+STATIC_PATHS = ['assets']
+THEME = ("theme/blue-penguin")
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
